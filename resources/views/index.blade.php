cdca962ace48beda1896d19e5d4150214fb5e505
potap <a-potap@mail.ru>
2025-11-01 19:14:30 +0400

    fix mobile link home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,7 +35,7 @@
             <div class="row">
                 <nav class="navbar navbar-expand-sm navbar-dark">
                     <div class="container-fluid">
-                        <a class="navbar-brand d-block d-sm-none" href="#">POTAPOV</a>
+                        <a class="navbar-brand d-block d-sm-none" href="/">POTAPOV</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>

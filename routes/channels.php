4068d8c8afdafabffab0b6e19e67c430a584c8bc
potap <a-potap@mail.ru>
2026-07-18 00:27:37 +0400

    setting reverb and update laravel version

--- routes/channels.php
+++ routes/channels.php
@@ -2,27 +2,6 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
-
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
-});
-
-Broadcast::channel('chat-room.{roomId}', function ($user, $roomId) {
-    $chatRoom = \App\Models\ChatRoom::find($roomId);
-
-    if (!$chatRoom) {
-        return false;
-    }
-
-    return $chatRoom->hasParticipant($user);
 });
